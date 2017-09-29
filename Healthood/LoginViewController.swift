bc92fb3d4ff70838951007b12e4afd108f499e1d
Krystian Bujak <Booyac@MacBook-Pro-macbook.local>
2017-09-29 13:37:32 +0200

    Added registerVC

--- Healthood/LoginViewController.swift
+++ Healthood/LoginViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
